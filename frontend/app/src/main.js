8ab07dda7b709082d623f4ecee182868b5164004
Benedikt Wagenr <benedikt.wagner@stud-mail.uni-wuerzburg.de>
2020-11-29 20:40:57 +0100

    docker

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -1,26 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from 'vuex'
 import router from "./router/router";
 import "bootstrap/dist/css/bootstrap.css";
 import '@/assets/css/main.css'
 import Axios from 'axios';
-
-import VueJWT from 'vuejs-jwt'
 import vuetify from './plugins/vuetify';
 
 Axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT;
-
 Vue.prototype.$axios = Axios
-Vue.prototype.$jwt = VueJWT
 Vue.config.productionTip = false
 
 
 new Vue({
   router,
   Axios,
-  Vuex,
-  VueJWT,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
